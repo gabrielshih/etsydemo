250a1d71a23aea0970cc27a6dc71e27d76ee8656
R. Gabriel Shih <gabrielshih@gmail.com>
2014-08-08 14:53:54 -0700

    Adding other files forgotten in last one

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -14,4 +14,9 @@ class Listing < ActiveRecord::Base
   validates_attachment_presence :image
 
   belongs_to :user
+
+  # This is to make it so that each listing can have multiple orders (if Seller has more than
+  #   one of the product). Might want to get rid of this later on if there is only one item
+  #   per listing (which is most likely for a video game).
+  has_many :orders
 end
